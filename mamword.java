75ae061e842e7529060d6ee2710458faeabc614f
sharath <124514826+SpSkp@users.noreply.github.com>
2023-10-28 10:40:24 +0530

    Update mamword.java

--- mamword.java
+++ mamword.java
@@ -1,7 +1,7 @@
-class Simple{  
+class Double{  
     public static void main(String args[]){  
-     System.out.println("Hello man team with sharath team1");
-      System.out.println("Hello Batch WITH FOR STUDENTS RUNNING in front of Ujwal (offer) and Aniket and Kumar sindhu in making CI automated vikas teach devops CI proecss"); 
-       System.out.println("Hello shan it a shandar");
+     System.out.println("Hello Students from Batch 30"); 
+      System.out.println("bal baccha heitho saccha");
+      System.out.println("Road me jatha he mal gadi fir blotha he durgigudi");
     }  
 } 
